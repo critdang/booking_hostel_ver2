2f9ab08f7e46a7740efe853f6d4a8d31145befb2
Dang Quoc Huy <huy.dang@madison-technology.com>
2022-11-05 14:13:11 +0700

    upload

--- src/utils/helperFn.js
+++ src/utils/helperFn.js
@@ -28,3 +28,32 @@ exports.hashPassword = async (password) => {
   const hashPassword = await bcrypt.hash(password, salt);
   return hashPassword;
 };
+
+exports.sendMail = async (
+  email,
+  subject,
+  text,
+  endpoint = '/',
+  token = '',
+) => {
+  const domain = 'http://localhost:3000';
+
+  const transporter = nodemailer.createTransport({
+    host: 'smtp.mailtrap.io',
+    port: 2525,
+    auth: {
+      user: process.env.AUTH_USER,
+      pass: process.env.AUTH_PASS,
+    },
+  });
+  const link = `${domain + endpoint + token}`;
+  const data = await ejs.renderFile('./src/views/createVerifyNoti/verify.ejs', { link });
+
+  const mailOption = {
+    from: process.env.EMAIL,
+    to: email,
+    subject,
+    html: data,
+  };
+  await transporter.sendMail(mailOption);
+};
