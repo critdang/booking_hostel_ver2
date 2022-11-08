87b8f21b51d8ef78abfb124c4235b76b9a813b6a
Dang Quoc Huy <huy.dang@madison-technology.com>
2022-11-08 07:22:03 +0700

    update seeder

--- src/route/index.js
+++ src/route/index.js
@@ -12,6 +12,17 @@ const initRoutes = (app) => {
   app.use('/category', categoryRouter);
   app.use('/cart', cartRouter);
   app.use('/order', orderRouter);
+
+  // Handle not found URL errors
+  app.use("*", (req, res) => {
+    console.log("🚀 ~ file: index.js ~ line 18 ~ app.use ~ req", req.orginalUrl);
+    const err = Error(`Requested path ${req.path} not found`);
+    res.status(404).send({
+      success: false,
+      message: `Requested path ${req.path} not found`,
+      stack: err.stack,
+    });
+  });
 };
 
 module.exports = initRoutes;
