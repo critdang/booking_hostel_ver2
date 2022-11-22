27ae0480d86dd05828a7ff737f0e876bdc97965b
Dang Quoc Huy <huy.dang@madison-technology.com>
2022-11-22 08:43:35 +0700

    update

--- src/route/auth.router.js
+++ src/route/auth.router.js
@@ -4,6 +4,11 @@ const controller = require("../controller/admin.controller");
 
 const router = express.Router();
 
+// getting login URL
+
+// getting the user from google with key
+
+// getting current user
 router.get("/verify/:token", controller.verifyUser);
 router.get("/forgotPassword/verify/:token", controller.verifyResetPassword);
 router.post("/resetPassword/:tokenId", controller.resetPassword);
