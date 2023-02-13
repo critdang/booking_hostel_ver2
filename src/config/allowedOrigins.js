b2c893e2dc5cfdda0a5c6f4f048df3e8fe0dc541
Dang Quoc Huy <huy.dang@madison-technology.com>
2023-02-13 14:51:09 +0700

    commit

--- src/config/allowedOrigins.js
+++ src/config/allowedOrigins.js
@@ -2,10 +2,12 @@ const allowedOrigins = process.env.NODE_ENV === 'production'
   ? [
     'https://booking-hotel-fe.vercel.app',
     'https://www.critdang.tech',
-    'http://localhost:3000'
+    'http://localhost:3001'
   ] : [
     // Add your FE domains allowed origins here
     'http://localhost:3000',
+    'http://localhost:3001',
+    'http://localhost:19006'
   ];
 
 module.exports = allowedOrigins;
